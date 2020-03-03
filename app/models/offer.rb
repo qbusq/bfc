234a8418a819032f89f8683afdd9df679d9e1065
Eva Karlsdottir <evakarlsdottir@gmail.com>
2020-03-03 15:15:12 +0100

    validation added for offer and user models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,4 +4,5 @@ class Offer < ApplicationRecord
   belongs_to :offer_request_id, class_name: 'Offer'
   has_many :offer_lines, dependent: :destroy
   validates :from_user, :to_user, :date, :start_date, :end_date, :status, presence: true
+  validates :status, inclusion: { in: ["requested", "sent", "pending", "accepted", "confirmed", "declined"] }
 end
