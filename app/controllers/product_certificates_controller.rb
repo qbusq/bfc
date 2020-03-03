06e18fc4c41adfe8da5d7ac494d7a797ca83bf17
Eva Karlsdottir <evakarlsdottir@gmail.com>
2020-03-03 14:21:53 +0100

    routes and controllers created

---
+++ app/controllers/product_certificates_controller.rb
@@ -0,0 +1,2 @@
+class ProductCertificatesController < ApplicationController
+end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
