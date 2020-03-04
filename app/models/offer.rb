a24581b348e31496dc213afb50d5c705deefc157
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2020-03-04 16:54:15 +0100

    Did some stuff but need to do more stuff

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,5 @@ class Offer < ApplicationRecord
   has_many :offer_lines, dependent: :destroy
   validates :from_user, :to_user, :date, :start_date, :end_date, :status, presence: true
   validates :status, inclusion: { in: ["requested", "sent", "pending", "accepted", "confirmed", "declined"] }
+  has_one_attached :offer_attachment
 end
