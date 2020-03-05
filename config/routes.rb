31e6493bc3a865bde17b4a91fa06fe182b11f969
Merlin Matheys <merlinmatheys@gmail.com>
2020-03-05 15:23:47 +0100

    New routes and views added and the links from the user home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   resources :user do
     resources :company_certificates, only: [:new, :create, :destroy]
+    collection do
+      get 'my_products'
+    end
   end
   root to: 'pages#home'
   resources :offers, except: [:edit, :update] do
     resources :offer_lines, only: [:new, :create, :index, :destroy]
     resources :offers, except: [:edit, :update]
+    collection do
+      get 'offers_requested'
+      get 'offers_received'
+      get 'contracts'
+    end
   end
   resources :products do
     resources :product_certificates, only: [:new, :create, :destroy]
