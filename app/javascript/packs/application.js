71cb2bc9558546da87e11b896b1cc546bc8959cf
Merlin Matheys <merlinmatheys@gmail.com>
2020-03-06 16:06:27 +0100

    Box clickable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@ import "bootstrap";
 
 import {initUpdateNavbarOnScroll} from "../components/navbar";
 import {initUpdateNavbar} from "../components/navbar";
+import {boxMadeClickable} from "../components/user_show";
+boxMadeClickable();
+
 
 if (window.location.pathname=='/') {
   initUpdateNavbarOnScroll()
