2bc157bedc3f46d8e43e8cbe1851b76536fa52d0
Merlin Matheys <merlinmatheys@gmail.com>
2020-03-09 13:02:50 +0100

    navbar improved

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,19 @@
 const initUpdateNavbarOnScroll = () => {
   const height = document.getElementById('banner').offsetHeight;
   const navbar = document.querySelector('.navbar');
+  const navbar_items = document.querySelectorAll('.navbar-item');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= height) {
         navbar.classList.add('navbar-white');
+        navbar_items.forEach((navbar_item) => {
+        navbar_item.classList.add('navbar-item-black');
+        });
       } else {
         navbar.classList.remove('navbar-white');
+        navbar_items.forEach((navbar_item) => {
+        navbar_item.classList.remove('navbar-item-black');
+        });
       }
     });
   }
@@ -14,11 +21,16 @@ const initUpdateNavbarOnScroll = () => {
 
 const initUpdateNavbar = () => {
   const navbar = document.querySelector('.navbar');
+  const navbar_items = document.querySelectorAll('.navbar-item');
   if (navbar) {
     navbar.classList.add('navbar-white-unfixed');
+    navbar_items.forEach((navbar_item) => {
+      navbar_item.classList.add('navbar-item-black');
+    });
   }
 }
 
 export { initUpdateNavbarOnScroll };
 export { initUpdateNavbar };
+
 
